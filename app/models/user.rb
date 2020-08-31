369254c7659faf655e41df5f7ceca72cb5c04d24
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-08-31 14:48:11 -0300

    implementing associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :orders
+  has_many :reviews, through: :orders
+  has_one :selling_inventory, class_name: 'Inventory', foreign_key: 'user_id'
+  has_many :buying_inventories, through: :orders, source: :inventory
 end
