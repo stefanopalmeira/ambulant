1eb6a59c0d138c54e62a000bdf1c598c69e194a6
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-08-31 15:37:47 -0300

    implementing cloudinaro

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  protected 
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :lat, :long, :seller :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :lat, :long, :seller :photo])
+  end
 end
