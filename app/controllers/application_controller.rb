d0fec4ae305942e537716cfb0bf16fcfb8d81823
Diego Oliveira <diegomaka@gmail.com>
2020-09-01 17:59:10 -0300

    Now seed with address

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :bio, :lat, :long, :seller, :photo])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :bio, :lat, :long, :seller, :photo])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :bio, :lat, :long, :seller, :photo])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :bio, :lat, :long, :seller, :photo])
   end
 end
