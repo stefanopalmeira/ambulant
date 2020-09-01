bf51c59f58c8f1ef512229101bd8f7ea5ef3c821
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-09-01 12:32:29 -0300

    implementing pundit

--- app/controllers/inventories_controller.rb
+++ app/controllers/inventories_controller.rb
@@ -1,2 +1,55 @@
 class InventoriesController < ApplicationController
+  before_action :set_inventory, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: :show
+
+ def show
+  authorize @inventory
+ end
+
+ def new
+  @inventory = Inventory.new
+  authorize @inventory
+ end
+
+ def create
+  @inventory = Inventory.new(inventory_params)
+  @inventory.selling_user = current_user
+  if @inventory.save
+    redirect_to inventory_path(@inventory)
+  else
+    render :new
+  end
+
+ end
+
+ def edit
+  authorize @inventory
+ end
+
+ def update
+  authorize @inventory 
+  if @inventory.update(inventory_params)
+    redirect_to inventory_path(@inventory)
+  else
+    render :edit
+  end
+ end
+
+ def destroy
+  authorize @inventory
+  @inventory.destroy
+  redirect_to root_path
+ end
+
+
+ private
+
+ def set_inventory
+  @inventory = Inventory.find(params[:id])
+ end 
+
+ def inventory_params
+  params.require(:inventory).permit(:name, :description, :photos [])
+ end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users 
+
+  resources :inventories, except: :index do
+    resources :order, only: [:show, :destroy, :create]
+  end
+  
+ 
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
