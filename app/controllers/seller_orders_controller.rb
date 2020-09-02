99843e80d9f8ed2eb7a7e0e20d2fcee7a7ae3de2
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-09-02 20:32:35 -0300

    implementing buyer order update

--- app/controllers/seller_orders_controller.rb
+++ app/controllers/seller_orders_controller.rb
@@ -6,12 +6,26 @@ class SellerOrdersController < ApplicationController
   end
 
   def update
-    authorize @order, policy_class: BuyerOrdersPolicy 
+    @order = Order.find(params[:id])
+    status = params[:accepted].to_i
+    if status == 2
+      @order.accepted = 2
+      authorize @order, policy_class: SellerOrdersPolicy
+      if @order.save
+        redirect_to seller_orders_path
+      end
+    elsif status == 3
+      @order.accepted = 3
+      authorize @order, policy_class: SellerOrdersPolicy
+      if @order.save
+        redirect_to seller_orders_path
+      end
+    end 
   end
 
   private
 
   def check_seller
-    redirect_to buyer_orders_path unless current_user.seller
+    redirect_to root_path unless current_user.seller
   end
 end
--- app/policies/seller_orders_policy.rb
+++ app/policies/seller_orders_policy.rb
@@ -1,7 +1,7 @@
 class SellerOrdersPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope.select('orders.*').joins(inventory: :selling_user).where(inventories: { selling_user: user }).order(created_at: :desc)
+      scope.select('orders.*').joins(inventory: :selling_user).where(inventories: { selling_user: user }).order(created_at: :asc)
     end
   end
 
