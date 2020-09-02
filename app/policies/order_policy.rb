55e657510cfa1c043481422cd4e1e1ffc4cc826c
Stefano Palmeira <stefanopalmeira@gmail.com>
2020-09-02 15:09:25 -0300

    orders index WIP

--- app/policies/order_policy.rb
+++ app/policies/order_policy.rb
@@ -2,7 +2,7 @@ class OrderPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
       if user.seller
-        @orders = scope.all.where(inventory:{ selling_user: user })
+        @orders = scope.joins(inventory: :selling_user).where(user: user)
       else
         false
       end
