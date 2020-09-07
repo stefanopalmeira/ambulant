5ee9c17093d0fd21c8ef3ffc8212ee3769e21af1
Diego Oliveira <diegomaka@gmail.com>
2020-09-07 14:06:36 -0300

    Fixed user sign up, map center on user, stars on rating and custom review message

--- db/migrate/20200831165813_add_columns_to_user.rb
+++ db/migrate/20200831165813_add_columns_to_user.rb
@@ -2,7 +2,7 @@ class AddColumnsToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :name, :string
     add_column :users, :bio, :text
-    add_column :users, :seller, :boolean
+    add_column :users, :seller, :boolean, default: false
     add_column :users, :lat, :float
     add_column :users, :long, :float
   end
