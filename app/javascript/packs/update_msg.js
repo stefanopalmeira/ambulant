6f8825f034ea5a3e6c7a619f847c5f422c18c547
rafaelrocha <rafaelmachadodarocha@gmail.com>
2020-09-08 19:20:28 -0300

    checknomsgs

--- app/javascript/packs/update_msg.js
+++ app/javascript/packs/update_msg.js
@@ -1,6 +1,7 @@
 const updateMessage = () => {
   const checkButton = document.getElementById('button-seller-home');
   const checkMsg = document.querySelector('.btn-msg');
+  const checkNoMsg = document.querySelector('.btn-no-message')
   if (checkButton) {
     const sellerUrl = document.getElementById('seller-view').dataset.sellerUrl;
     setInterval(()=>{
@@ -9,6 +10,12 @@ const updateMessage = () => {
         .then(jsonResponse => {
           if (checkMsg) { 
           checkMsg.innerHTML = `Mensagens: ${jsonResponse['msg']}`;
+          }
+          if (checkNoMsg) {
+            if (jsonResponse['msg'] > 0)
+            checkNoMsg.innerHTML = `Mensagens: ${jsonResponse['msg']}`;
+            checkNoMsg.classList.add('btn-danger');
+            checkNoMsg.classList.remove('btn-secondary');
           }        
         });
     }, 10000)
